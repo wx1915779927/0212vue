42e5e8cba7e62141bea8e757216b7fea131e9461
wuxi <1915779927@qq.com>
2020-06-01 21:21:32 +0800

    'username-Ajax'

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,19 @@
 <template>
-  <div>
-    <header class="site-header jumbotron">
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-12">
-            <h1>请发表对Vue的评论</h1>
-          </div>
-        </div>
-      </div>
-    </header>
-    <div class="container">
-      <Add :addComment="addComment" />
-      <List :comments="comments" :deleteComment="deleteComment" />
-    </div>
+  <div class="container">
+    <Search />
+    <Main />
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import Add from "./components/Add";
-import List from "./components/List";
+import Search from './compones_test/Search'
+import Main from './compones_test/Main'
 export default {
-  components: {
-    Add,
-    List
-  },
-  data(){
-    return{
-      comments:[
-        {id:1,username:'彭于晏',content:'吴熙可太帅了'},
-        {id:2,username:'刘德华',content:'全村的骄傲'},
-        {id:3,username:'胡歌',content:'全村的希望'},
-      ]
-    }
-  },
-  methods:{
-    addComment(obj){
-      this.comments.unshift(obj)
-    },
-    deleteComment(index){
-      this.comments.splice(index,1)
-    }
+  components:{
+    Search,
+    Main
   }
-};
+}
 </script>
 
 <style scoped></style>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue' //把组装好的整体组件导入到入口文件，准备渲染
+import App from './App.vue'
+import "@babel/polyfill";
 Vue.config.productionTip = false
 new Vue({
+    beforeCreate() {
+        Vue.prototype.$bus = this
+    },
     el: '#root',
-    // render:function(createElement){
-    //     createElement(App)
-    // }
-    // render:createElement => createElement(App)
     render: h => h(App)
-    //第一步：把导入过来的App组件配置对象，在vue模板当中注册解析为一个标签名<App/>并使用
-    //第二步：把这个标签在模板当中进行渲染
 
 })
